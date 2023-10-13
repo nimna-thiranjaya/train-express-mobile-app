2cebcddbd533a22094877547e89469c184d2a8b3
Ravindu Sandeepana <89466778+ravindu-sandeepana@users.noreply.github.com>
2023-10-13 14:03:02 +0530

    Done all the parts of reservations

--- app/src/main/java/com/example/trainbookingapp/Reservation_details.java
+++ app/src/main/java/com/example/trainbookingapp/Reservation_details.java
@@ -2,7 +2,12 @@ package com.example.trainbookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.trainbookingapp.Auth.SignInActivity;
 
 public class Reservation_details extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Reservation_details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_details);
+        Button addRes = findViewById(R.id.ReservationBtn);
+
+
+        addRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AddReservationActivity.class);
+                startActivity(i);
+            }
+
+
+        });
     }
 }
